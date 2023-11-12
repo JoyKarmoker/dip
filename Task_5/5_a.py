c47a1a29104a342638ec3d6932b81bf9487c23f2
JoyKarmoker <Joykarmoker353@gmail.com>
2023-11-12 11:05:52 +0600

    Extra image removed

--- Task_5/5_a.py
+++ Task_5/5_a.py
@@ -4,25 +4,27 @@ import cv2
 
 def perform_erosion(image, structuring_element_size):
     row, cols = image.shape
+    kernel = np.ones((structuring_element_size, structuring_element_size), np.uint8)
     eroded_image = np.zeros(image.shape)
 
     for i in range(structuring_element_size//2, row-structuring_element_size//2):
         for j in range(structuring_element_size//2, cols-structuring_element_size//2):
             region = image[i-structuring_element_size//2:i+structuring_element_size//2+1,
                      j-structuring_element_size//2:j+structuring_element_size//2+1]
-            eroded_image[i, j] = np.min(region)
+            eroded_image[i, j] = np.min(region*kernel)
 
     return eroded_image
 
 def perform_dilation(image, structuring_element_size):
     row, cols = image.shape
+    kernel = np.ones((structuring_element_size, structuring_element_size), np.uint8)
     eroded_image = np.zeros(image.shape)
 
     for i in range(structuring_element_size//2, row-structuring_element_size//2):
         for j in range(structuring_element_size//2, cols-structuring_element_size//2):
             region = image[i-structuring_element_size//2:i+structuring_element_size//2+1,
                      j-structuring_element_size//2:j+structuring_element_size//2+1]
-            eroded_image[i, j] = np.max(region)
+            eroded_image[i, j] = np.max(region*kernel)
 
     return eroded_image
 
--- Task_5/5_b.py
+++ Task_5/5_b.py
@@ -4,26 +4,28 @@ import cv2
 
 def perform_erosion(image, structuring_element_size):
     row, cols = image.shape
+    kernel = np.ones((structuring_element_size, structuring_element_size), np.uint8)
     eroded_image = np.zeros(image.shape)
 
     for i in range(structuring_element_size//2, row-structuring_element_size//2):
         for j in range(structuring_element_size//2, cols-structuring_element_size//2):
             region = image[i-structuring_element_size//2:i+structuring_element_size//2+1,
                      j-structuring_element_size//2:j+structuring_element_size//2+1]
-            eroded_image[i, j] = np.min(region)
+            eroded_image[i, j] = np.min(region*kernel)
 
     return eroded_image
 
 
 def perform_dilation(image, structuring_element_size):
     row, cols = image.shape
+    kernel = np.ones((structuring_element_size, structuring_element_size), np.uint8)
     eroded_image = np.zeros(image.shape)
 
     for i in range(structuring_element_size//2, row-structuring_element_size//2):
         for j in range(structuring_element_size//2, cols-structuring_element_size//2):
             region = image[i-structuring_element_size//2:i+structuring_element_size//2+1,
                      j-structuring_element_size//2:j+structuring_element_size//2+1]
-            eroded_image[i, j] = np.max(region)
+            eroded_image[i, j] = np.max(region*kernel)
 
     return eroded_image
 
@@ -40,9 +42,9 @@ def perform_closing(image, structuring_element_size):
     return  closed_image
 
 original_image = cv2.imread('structure.png', cv2.IMREAD_GRAYSCALE)
-structuring_element_size = 50
+structuring_element_size = 51
 opened_image = perform_opening(original_image, structuring_element_size)
-structuring_element_size = 30
+structuring_element_size = 31
 closed_image = perform_closing(original_image, structuring_element_size)
 
 plt.figure(figsize=(8, 8))
